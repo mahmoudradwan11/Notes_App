bcd9f4edd24b6efa79734beb156e6ec14b08fe3c
mahmoudradwan11 <102020555+mahmoudradwan11@users.noreply.github.com>
2023-09-25 22:00:12 +0300

    create FloatingActionButton

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes/views/widgets/custom_app_bar.dart';
+import 'package:notes/views/widgets/custom_note_item.dart';
 import 'package:notes/views/widgets/custom_notes_ListView.dart';
 
 class NotesViewBody extends StatelessWidget {
